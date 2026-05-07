b0ebacb7d5143513da84b4480d92fb4186619b2a
luisjcm <ljcm47@gmail.com>
2026-05-06 20:06:30 -0400

    test 5

--- docs/assets/index-ClYNWKsn.js
+++ docs/assets/index-ClYNWKsn.js
@@ -677,7 +677,7 @@ Este proyecto consistió en replicar fielmente el diseño y la funcionalidad de 
           <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.49 4.49 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.49 4.49 0 01-1.307 3.497 4.49 4.49 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.49 4.49 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
         </svg>
         </h4>
-        <p class="text-xs md:text-sm text-zinc-400">UCV - Diplomadosonline.com | 2024 — 2025</p>
+        <p class="text-xs md:text-sm text-zinc-400">UCV - Diplomadosonline.com | 2024 - 2025</p>
       </div>
 
       <div class="p-4 border border-zinc-800 rounded-xl bg-zinc-900/20">
@@ -695,7 +695,7 @@ Este proyecto consistió en replicar fielmente el diseño y la funcionalidad de 
           <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.49 4.49 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.49 4.49 0 01-1.307 3.497 4.49 4.49 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.49 4.49 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
         </svg>
         </h4>
-        <p class="text-xs md:text-sm text-zinc-400 ">Udemy - Completado en Abril 2026</p>
+        <p class="text-xs md:text-sm text-zinc-400 ">Udemy | Abril 2026</p>
       </div>
 
       
